70dc0f2b755f5607cf23cf0dbdcc571b01aa3dec
kameronwere <kameronwere@hotmail.com>
2025-04-30 14:30:49 -0600

    Updating db.

--- Gemfile
+++ Gemfile
@@ -43,6 +43,9 @@ gem "kamal", require: false
 # Add HTTP asset caching/compression and X-Sendfile acceleration to Puma [https://github.com/basecamp/thruster/]
 gem "thruster", require: false
 
+# Using gem to detect browser type
+gem 'browser'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :set_browser
+
+  def set_browser
+    @browser = Browser.new(request.user_agent)
+  end
+
+  helper_method :mobile_device?
+
+  def mobile_device?
+    @browser.device.mobile?
+  end
+
 end
