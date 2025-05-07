94c4d414cbae3bb27200f468dab1a93067bae868
kameronwere <kameronwere@hotmail.com>
2025-05-07 11:15:50 -0600

    CORS added. API Base controller added.

---
+++ app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::ApiController < ActionController::API
+
+  NOT_FOUND ="Not Found"
+
+  # protect_from_forgery with: :null_session
+
+  rescue_from ActiveRecord::RecordNotFound do
+    render json: { error: NOT_FOUND}, status: :not_found
+  end
+end
