34d7f873a3534ea4d3081ebf6500170d89ec987f
kameronwere <kameronwere@hotmail.com>
2025-06-08 13:04:06 -0600

    Correctin issue from daily forecast

--- app/services/noaa/forecast/hour_forecast.rb
+++ app/services/noaa/forecast/hour_forecast.rb
@@ -12,18 +12,13 @@ module Noaa
       end
 
       def call
-        # cached_response = Rails.cache.read(cache_key(CACHE_KEY))
-        # return successful({ "hour_data" => cached_response }) if cached_response.present?
-
         response = parse_response(noaa_response)
         return failed("Unable to retrieve forecast for #{latitude}, #{longitude}") if response.nil?
 
-        hourly = hourly_data(response["properties"]["forecastHourly"])
+        hourly = hourly_data(response["properties"]["forecastHourly"], latitude, longitude)
         return failed("Unable to retrieve hourly forecasts for #{latitude}, #{longitude}") if hourly.nil?
 
         hour_data =  hourly["properties"]["periods"].select { |period| period["startTime"] == start_time }.first
-
-        Rails.cache.write(cache_key(CACHE_KEY), hour_data, expires_in: 45.minutes)
         successful({ "hour_data" => hour_data })
       end
     end
