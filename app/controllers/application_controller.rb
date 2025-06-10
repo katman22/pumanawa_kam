54804f112f0a598db42b3f6a014c181f992bc86e
katman22 <kameronwere@hotmail.com>
2025-06-09 18:17:45 -0600

    Add internationlization. (#46)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  before_action :set_browser
+  before_action :set_browser, :set_locale
 
   def set_browser
     @browser = Browser.new(request.user_agent)
@@ -12,5 +12,13 @@ class ApplicationController < ActionController::Base
 
   def mobile_device?
     @browser.device.mobile?
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
