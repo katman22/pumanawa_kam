cd2bf7730ee5c3a2859cfeee422531de70c54663
Kam Were <kameronwere@hotmail.com>
2025-08-28 13:02:06 -0600

    Turbo patch (#97)

--- app/javascript/controllers/parking_profiles_controller.js
+++ app/javascript/controllers/parking_profiles_controller.js
@@ -2,26 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 
 export default class extends Controller {
-    static targets = ["button", "input", "effectiveFrom", "effectiveTo"]
-
-    select(event) {
-        event.preventDefault()
-        const link = event.target.closest("[data-value]")
-        if (!link) return
-
-        const value = link.dataset.value
-        const label = link.textContent.trim()
-
-        this.buttonTarget.textContent = label
-        this.inputTarget.value = value
-    }
+    // static targets = ["button", "input", "effectiveFrom", "effectiveTo"]
+    //
+    // select(event) {
+    //     event.preventDefault()
+    //     const link = event.target.closest("[data-value]")
+    //     if (!link) return
+    //
+    //     const value = link.dataset.value
+    //     const label = link.textContent.trim()
+    //
+    //     this.buttonTarget.textContent = label
+    //     this.inputTarget.value = value
+    // }
 
     connect() {
-        // Initialize button label from saved value
-        if (this.inputTarget.value) {
-            const item = this.element.querySelector(`[data-value="${this.inputTarget.value}"]`)
-            if (item) this.buttonTarget.textContent = item.textContent.trim()
-        }
+        // // Initialize button label from saved value
+        // if (this.inputTarget.value) {
+        //     const item = this.element.querySelector(`[data-value="${this.inputTarget.value}"]`)
+        //     if (item) this.buttonTarget.textContent = item.textContent.trim()
+        // }
     }
 }
 
