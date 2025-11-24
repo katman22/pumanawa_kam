e004071b3e7584e8c904c58478872338b6d34d7d
Kam Were <kameronwere@hotmail.com>
2025-11-23 21:27:09 -0700

    Migration fix (#124)

--- db/migrate/20251105174113_create_home_resorts.rb
+++ db/migrate/20251105174113_create_home_resorts.rb
@@ -1,5 +1,5 @@
 # db/migrate/20251110_add_kind_to_home_resorts.rb
-class AddKindToHomeResorts < ActiveRecord::Migration[8.0]
+class CreateHomeResorts < ActiveRecord::Migration[8.0]
   def change
     add_column :home_resorts, :kind, :integer, null: false, default: 0  # 0=subscribed, 1=free
     add_check_constraint :home_resorts, "kind IN (0,1)", name: "home_resorts_kind_check"
